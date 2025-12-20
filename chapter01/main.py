3a695a95bd69d95a5a35a6754bb565c9cbab349f
qe-present <2664481691@qq.com>
2025-12-20 22:22:14 +0800

    自动提交

--- chapter01/main.py
+++ chapter01/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8001)
+@app.get('/greet/{name}')
+async def greet(name: str)->dict:
+    return {"message": f"Hello, {name}!"}
