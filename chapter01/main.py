05851c8d1a38e3d06b7e772cbb241714bad903c8
qe-present <2664481691@qq.com>
2025-12-20 23:22:36 +0800

    自动提交

--- chapter01/main.py
+++ chapter01/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+from fastapi import FastAPI,Header
+
 app = FastAPI()
 
 
@@ -6,6 +10,39 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get('/greet/{name}')
-async def greet(name: str)->dict:
+
+@app.get("/greet1/{name}")
+async def greet(name: str):
     return {"message": f"Hello, {name}!"}
+
+
+# 可选参数
+@app.get('/greet/')
+async def greet(
+        name: Optional[str] = "john",
+        age: Optional[int] = 30
+) -> dict:
+    return {"message": f"Hello, {name}!", "age": age}
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+
+
+@app.post('/create_book')
+async def create_book(book: Book):
+    return {"title": book.title, "author": book.author}
+
+@app.get('/get_header')
+async def get_header(
+        accept:str=Header(None),
+        content_type=Header(None),
+        user_agent=Header(None)
+):
+    request_headers={}
+    request_headers['Accept']=accept
+    request_headers['Content-Type']=content_type
+    request_headers['User-Agent']=user_agent
+    return request_headers
+
